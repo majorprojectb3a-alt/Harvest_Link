088a333a4d7ea570aa27bab6bc174d5804809ae8
Shanmukhi Bolisetti <shanmukhi.bolisetti2004@gmail.com>
2026-03-04 19:44:03 +0530

    feat: landing page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/Landing/LandingPage";
 import BuyerHome from './pages/HomePages/BuyerHome';
 import FarmerHome from "./pages/HomePages/FarmerHome";
 import EditProfile from "./pages/EditProfile/EditProfile";
@@ -22,7 +23,7 @@ function App() {
   return (
       <Routes>
         <Route path = "/unauthorized" element = {<Unauthorized/>} />
-        <Route path="/" element={<FarmerAuth />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/BuyerHome" element={
           <RoleProtectedRoute allow = {['buyer']}>
             <BuyerHome />
